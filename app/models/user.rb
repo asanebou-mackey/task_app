27b3bbcb03b8badd799c158ba10a7cc4e0b8b7f0
asanebou.mackey <asanebou.job4219@gmail.com>
2022-11-07 00:36:36 +0900

    バリデーションエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+    validates :title, presence: true   #タイトル入力欄が空ではないか
+    validates :startday, presence: true   #開始日入力欄が空ではないか
+    validates :endday, presence: true   #終了日入力欄が空ではないか
+    validate :date_before_start
+    validate :date_before_finish
+
+    def date_before_start
+        return if startday.blank?
+        errors.add(:startday, "は今日以降のものを選択してください") if startday < Date.today
+      end
+    
+      def date_before_finish
+        return if endday.blank? || startday.blank?
+        errors.add(:endday, "は開始日以降のものを選択してください") if endday < startday
+      end
 end
